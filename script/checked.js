4e8d5e77fa75304ec86623e918972f35871ed741
Hleb Sharstuk <sharhleb@gmail.com>
2022-01-20 09:46:22 +0300

    Remove comments and log, exchange condition by adding elvis operator

--- script/checked.js
+++ script/checked.js
@@ -1,12 +1,10 @@
 const checkboxWrapper = document.querySelector('.todos-list');
 
-console.log(checkboxWrapper)
-
 checkboxWrapper.addEventListener('click', (e) => {
-    //wait click event in any checkbox
-    if(e.target && e.target.matches('input[type=checkbox]')) {
+    const currentTitle = e.target.nextElementSibling;
+
+    if(e.target?.matches('input[type=checkbox]')) {
         e.target.setAttribute('disabled', 'disabled')
-        const currentTitle = e.target.nextElementSibling; // the title of the corresponding checkbox
         currentTitle.classList.add('crosstext');
     }
 })
