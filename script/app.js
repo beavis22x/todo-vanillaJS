11b5066d6be60145681230163e3529ea5b379ca6
Hleb Sharstuk <sharhleb@gmail.com>
2022-01-25 11:48:47 +0300

    Refactored removeTask.js: added filter and closest method, improved code style

--- script/app.js
+++ script/app.js
@@ -18,6 +18,7 @@ const useState = (defaultValue) => {
     let value = defaultValue;
     const getValue = () => value
     const setValue = newValue => value = newValue
+
     return [getValue, setValue];
 }
 
--- script/modules/removeTask.js
+++ script/modules/removeTask.js
@@ -1,11 +1,10 @@
 export const removeTask = function deleteTask({e, taskList, setTaskList, renderTaskList}) {
     if (e.target?.matches('button.delete-item-btn')) {
-        const item = e.target.parentElement.parentElement;
+        const item = e.target.closest('article');
         const itemId = Number(item.getAttribute('id'))
-        const index = taskList().findIndex(elem => elem.id === itemId);
 
         item.remove()
-        setTaskList([...taskList().slice(0, index), ...taskList().slice(index + 1)]);
+        setTaskList([...taskList().filter(item => item.id !== itemId)])
         renderTaskList();
     }
 }
