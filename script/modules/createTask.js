c1c76617d0a5ea762ceb480c94111eed09e00df4
Hleb Sharstuk <sharhleb@gmail.com>
2022-01-26 16:00:11 +0300

    exchange nfe on arrow func in create-, edit-, remove-, renderTask, setTime; refactor destructuring in createTask

--- script/modules/createTask.js
+++ script/modules/createTask.js
@@ -1,24 +1,27 @@
 let taskId = 1;
 
-export const createTaskObj = function addTask({e, formTitle, formEndField, taskList, setTaskList, formStartField, closeModalFunc}) {
-    const {
-        formTitle: {value: title},
-        formStartField: {value: start},
-        formEndField: {value: end}
-    } = {formTitle, formStartField, formEndField}
-
+export const createTaskObj = ({
+                                  e,
+                                  setTaskList,
+                                  taskList,
+                                  closeModalFunc,
+                                  formTitle: {value: title},
+                                  formStartField: {value: start},
+                                  formEndField: {value: end},
+                              }
+) => {
     const taskObj = {
         id: taskId,
         title,
         start,
         end,
         completed: false,
-        editMode:false,
+        editMode: false,
     };
 
-    if(title === '') return closeModalFunc(e)
+    if (title === '') return closeModalFunc(e)
 
-    setTaskList([...taskList(),taskObj])
+    setTaskList([...taskList(), taskObj])
     taskId++;
     closeModalFunc(e)
 }
--- script/modules/editTask.js
+++ script/modules/editTask.js
@@ -1,10 +1,10 @@
-export const editTask = function editItem({e, EDIT_ATTRIBUTE, modalContainer, openModalFunc}) {
+export const editTask = ({e, EDIT_ATTRIBUTE, modalContainer, openModalFunc}) => {
     if (e.target?.matches('button.edit-item-btn')) {
         const currentItem = e.target.closest('article');
         const editId = Number(currentItem.getAttribute('id'));
-        const formTitle = currentItem.children[1].lastElementChild.innerHTML;
-        const formStartField = currentItem.children[2].lastElementChild.innerHTML;
-        const formEndField = currentItem.children[3].lastElementChild.innerHTML;
+        const formTitle = currentItem.querySelector('.title span').innerHTML
+        const formStartField = currentItem.querySelector('.start-time span').innerHTML;
+        const formEndField = currentItem.querySelector('.end-time span').innerHTML;
 
         modalContainer.setAttribute(EDIT_ATTRIBUTE, editId);
         openModalFunc({e, title: formTitle, start: formStartField, end: formEndField})
